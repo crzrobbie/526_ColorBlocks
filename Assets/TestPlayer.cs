da48cdad3d7d081d3947cfdeb16f974821fd50fa
bertram-zbc <Bertram.zbc@gmail.com>
2019-03-22 19:27:20 -0700

    photon

--- Assets/TestPlayer.cs
+++ Assets/TestPlayer.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System;
 
 public class TestPlayer : MonoBehaviourPunCallbacks, IPunObservable
 {
     public float speed = 1f;
     public PhotonView photonView;
+    public GameObject body;
+
     Vector2 dir = Vector2.right;
 
-    public GameObject body;
+    List<Transform> tail = new List<Transform>();
+    List<Transform> domain = new List<Transform>();
+
+    bool inDomain = false;
+
+    float minX;
+    float minY;
+    float maxX;
+    float maxY;
+
 
     // Start is called before the first frame update
     void Start()
     {
         if(photonView.IsMine)
         {
+            minX = transform.position.x;
+            maxX = transform.position.x;
+            minY = transform.position.y;
+            maxY = transform.position.y;
+            createDomain(); //create start block
             InvokeRepeating("Move", speed, speed);
         }
         
@@ -40,10 +57,101 @@ public class TestPlayer : MonoBehaviourPunCallbacks, IPunObservable
 
     void Move()
     {
-        Vector2 vector = transform.position;
         transform.Translate(dir);
-        PhotonNetwork.Instantiate(body.name, vector, Quaternion.identity, 0);
-        
+        Vector2 vector = transform.position;
+        updateField(vector);
+        bool isContain = isContains(domain, vector);
+        if (!inDomain && isContain)
+        {
+            fill();
+            inDomain = true;
+        }
+        if (!isContain)
+        {
+            GameObject trace = PhotonNetwork.Instantiate(body.name, vector, Quaternion.identity, 0);
+            tail.Insert(0, trace.transform);
+            inDomain = false;
+        }
+
+    }
+
+    private void fill()
+    {
+        domain.InsertRange(0, tail);
+        tail.Clear();
+        Hashtable xTable = new Hashtable();
+        Hashtable yTable = new Hashtable();
+        foreach (Transform t in domain)
+        {
+            float x = t.position.x;
+            float y = t.position.y;
+            if (xTable.ContainsKey(x))
+            {
+                List<Transform> xList = (List<Transform>)xTable[x];
+                xList.Add(t);
+            }
+            else
+            {
+                List<Transform> list = new List<Transform>();
+                list.Add(t);
+                xTable.Add(x, list);
+            }
+            if (yTable.ContainsKey(y))
+            {
+                List<Transform> yList = (List<Transform>)yTable[y];
+                yList.Add(t);
+            }
+            else
+            {
+                List<Transform> list = new List<Transform>();
+                list.Add(t);
+                yTable.Add(y, list);
+            }
+        }
+        for (float i = minX; i <= maxX; i++)
+        {
+            for (float j = minY; j <= maxY; j++)
+            {
+                List<Transform> xList = (List<Transform>)xTable[i];
+                List<Transform> yList = (List<Transform>)yTable[j];
+                if (isInDomain(xList, yList, i, j))
+                {
+                    Vector2 vector = new Vector2(i, j);
+                    PhotonNetwork.Instantiate(body.name, vector, Quaternion.identity, 0);
+                    //domain.Insert(0, body.transform);
+                }
+
+            }
+        }
+    }
+
+    private bool isInDomain(List<Transform> xList, List<Transform> yList, float x, float y)
+    {
+        if (xList.Count == 0 || yList.Count == 0) return false;
+        float minY = xList[0].position.y, maxY = xList[0].position.y;
+        foreach (Transform t in xList)
+        {
+            minY = Math.Min(minY, t.position.y);
+            maxY = Math.Max(maxY, t.position.y);
+        }
+        float minX = yList[0].position.x, maxX = yList[0].position.x;
+        foreach (Transform t in yList)
+        {
+            minX = Math.Min(minX, t.position.x);
+            maxX = Math.Max(maxX, t.position.x);
+        }
+        if (x >= minX && x <= maxX && y >= minY && y <= maxY) return true;
+        return false;
+    }
+
+    private bool isContains(List<Transform> domain, Vector2 v)
+    {
+        foreach (Transform t in domain)
+        {
+            if (t.position.x == v.x && t.position.y == v.y)
+                return true;
+        }
+        return false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -67,5 +175,31 @@ public class TestPlayer : MonoBehaviourPunCallbacks, IPunObservable
             transform.position = (Vector3)stream.ReceiveNext();
         }
     }
-    
+
+    void createDomain()
+    {
+        Vector2 vector = transform.position;
+        int width = 3;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Vector2 v = new Vector2(vector.x - i, vector.y + 1 - j);
+                updateField(v);
+                GameObject d = PhotonNetwork.Instantiate(body.name, v, Quaternion.identity, 0);
+                d.name = "domain";
+                domain.Insert(0, d.transform);
+                //Debug.Log(v);
+            }
+        }
+    }
+
+    private void updateField(Vector2 v)
+    {
+        minX = Math.Min(minX, v.x);
+        maxX = Math.Max(maxX, v.x);
+        minY = Math.Min(minY, v.y);
+        maxY = Math.Max(maxY, v.y);
+    }
+
 }
--- Assets/testGameManager.cs
+++ Assets/testGameManager.cs
@@ -11,9 +11,16 @@ public class testGameManager : MonoBehaviourPun
     int num = 0;
     public GameObject player;
     public GameObject body;
+
+    Color[] playerColor = { Color.green, Color.blue, Color.red, Color.yellow };
+
+
     // Start is called before the first frame update
     void Start()
     {
+        int playerNum = PhotonNetwork.CurrentRoom.PlayerCount;
+        player.GetComponent<SpriteRenderer>().color = playerColor[playerNum + 1];
+        body.GetComponent<SpriteRenderer>().color = playerColor[playerNum + 1];
         PhotonNetwork.Instantiate(this.player.name, new Vector3(0, 0, 0), Quaternion.identity, 0);
     }
 
@@ -23,10 +30,4 @@ public class testGameManager : MonoBehaviourPun
         
     }
 
-    public void addBodyObject(Vector2 vector)
-    {
-        PhotonNetwork.Instantiate(body.name, vector, Quaternion.identity, 0);
-        num++;
-        Debug.Log(num);
-    }
 }
